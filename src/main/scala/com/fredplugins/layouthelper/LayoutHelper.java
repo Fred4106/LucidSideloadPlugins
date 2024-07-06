f92ab97bcfe67f6d6f8ef32b86c907040adf154c
Nathaniel <natruf@gmail.com>
2024-07-06 17:30:34 -0500

    Added Layout helper plugin.

--- src/main/scala/com/fredplugins/layouthelper/LayoutHelper.java
+++ src/main/scala/com/fredplugins/layouthelper/LayoutHelper.java
@@ -9,13 +9,14 @@ import com.lucidplugins.api.utils.MessageUtils;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
-import net.runelite.api.NPC;
-import net.runelite.api.Projectile;
-import net.runelite.api.TileObject;
+import net.runelite.api.*;
 import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.callback.ClientThread;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDependency;
@@ -60,90 +61,40 @@ public class LayoutHelper extends Plugin {
     @Inject
     private ClientThread clientThread;
 
-    private OverlayWidgetHelper tab1 = null;
-    private OverlayWidgetHelper tab2 = null;
-    private OverlayWidgetHelper inventory = null;
+    @Inject
+    private ChatMessageManager chatMessageManager;
+    
     scala.collection.immutable.List<OverlayWidgetHelper> overlays = List$.MODULE$.empty();
+
+    private void sendMessage(ChatMessageType tpe, String message) {
+        chatMessageManager.queue(QueuedMessage.builder().type(tpe).runeLiteFormattedMessage(message).build());
+    }
 
     @Override
     protected void startUp() throws Exception {
         super.startUp();
 
-        scala.collection.immutable.List<OverlayWidgetHelper> overlays = OverlayWidgetHelper.getOverlays(overlayManager).filter(o ->
+        overlays = OverlayWidgetHelper.getOverlays(overlayManager).filter(o ->
             o.groupId() == 164 && o.childId() >= 94 && o.childId() <= 96
         );
         overlays.foreach(o -> {
             o.snappable_$eq(false);
-            MessageUtils.addMessage(
-                    "[" + o.groupId() + ":" + o.childId() + "] [" + o.name() + "]\n\t[" + o.preferredPosition() + "]\n\t[" + o.preferredLocation() + "]\n\t[snap: " + o.snappable() + ", moveable: " + o.movable() + ", resizeable: " + o.resizable() + "]",
-                    Color.RED
-            );
+            sendMessage(ChatMessageType.PRIVATECHAT, o.pretty(true));
             return -1;
         });
-//        for (OverlayWidgetHelper o : overlays) {
-//            if(!o.movable() || !o.resettable()) {
-//                String message = "[" + o.groupId() + ":" + o.childId() + "] [" + o.name() + "] [" + o.preferredPosition() + "] [" + o.preferredLocation() + "]";
-//                MessageUtils.addMessage(message, Color.RED);
-//            }
-//        }
-        tab1 = overlays.find(o ->
-                o.groupId() == 164 && o.childId() == 94
-        ).get();
-
-        tab2 = overlays.find(o ->
-                o.groupId() == 164 && o.childId() == 95
-        ).get();
-
-        inventory = overlays.find(o ->
-                o.groupId() == 164 && o.childId() == 96
-        ).get();
     }
 
     @Subscribe
     private void onClientTick(ClientTick gt) {
-        boolean noMove = false;
-        if(tab1.moved()) {
-            if(tab1.preferredLocation() == null) {
-                tab2.reset();
-                inventory.reset();
-            }else {
-                tab2.preferredLocation_$eq(Tuple2.apply(tab1.preferredLocation().x, tab1.preferredLocation().y - 36));
-                inventory.preferredLocation_$eq(Tuple2.apply(tab1.preferredLocation().x + 14, tab1.preferredLocation().y - 273 - 36));
-            }
-        } else if(tab2.moved()) {
-            if(tab2.preferredLocation() == null) {
-                tab1.reset();
-                inventory.reset();
-            } else {
-                tab1.preferredLocation_$eq(Tuple2.apply(tab2.preferredLocation().x, tab2.preferredLocation().y + 36));
-                inventory.preferredLocation_$eq(Tuple2.apply(tab2.preferredLocation().x + 14, tab2.preferredLocation().y - 273));
-            }
-        } else if(inventory.moved()) {
-            if(inventory.preferredLocation() == null) {
-                tab1.reset();
-                tab2.reset();
-            } else {
-                tab1.preferredLocation_$eq(Tuple2.apply(inventory.preferredLocation().x - 14, inventory.preferredLocation().y + 273 + 36));
-                tab2.preferredLocation_$eq(Tuple2.apply(inventory.preferredLocation().x - 14, inventory.preferredLocation().y + 273));
-            }
-        } else {
-            noMove = true;
-        }
-        if(!noMove) {
-            MessageUtils.addMessage(" tab1: " + tab1.preferredLocation(), Color.WHITE);
-            MessageUtils.addMessage(" tab2: " + tab2.preferredLocation(), Color.GREEN);
-            MessageUtils.addMessage("panel: " + inventory.preferredLocation(), Color.BLUE);
-            tab2.cache();
-            tab1.cache();
-            inventory.cache();
-        }
+        OverlayWidgetHelper.tick(chatMessageManager, overlays);
     }
     @Override
     protected void shutDown() throws Exception {
         super.shutDown();
-        tab1 = null;
-        tab2 = null;
-        inventory = null;
+        overlays.foreach(o -> {
+            o.snappable_$eq(true);
+            return -1;
+        });
         overlays = List$.MODULE$.empty();
     }
 }
