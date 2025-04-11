16447c2d9a90a0489a22f18dacb82c760169d05a
Nathaniel <natruf@gmail.com>
2025-04-11 14:23:07 -0500

    Update to rev 130

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@
  */
 
 object Dependencies {
-    const val rlVersion = "1.11.4"
+    const val rlVersion = "1.11.6.1"
     const val scalaVersion = "3.6.3"
     const val lombokVersion = "1.18.30"
     const val jetbrainsAnnotations = "23.0.0"
