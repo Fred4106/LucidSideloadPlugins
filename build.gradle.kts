9a509f4d90b822438be826cb6f158eb7d499e837
Nathaniel <natruf@gmail.com>
2025-08-01 15:41:48 -0500

    Bump plugin version to 1.1.9 and rlVersion dependency to 1.11.13-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ val javaVersion = JavaVersion.VERSION_11
 
 allprojects {
     group = "com.fredplugins"
-    version = "1.1.8"
+    version = "1.1.9"
     repositories {
         mavenLocal()
         gradlePluginPortal()
