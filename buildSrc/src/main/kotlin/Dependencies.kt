a9dba02699cc37453503fa3df9902273d40a6e7c
Nathaniel <natruf@gmail.com>
2025-12-26 15:38:57 -0600

    - **buildSrc**   - Updated `rlVersion` in `Dependencies.kt` from `1.12.9-SNAPSHOT` to `1.12.10`.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object Dependencies {
     const val releaseVersion = "1.1.19"
-    const val rlVersion = "1.12.9-SNAPSHOT"
+    const val rlVersion = "1.12.10"
     const val scalaVersion = "3.7.4"
     const val lombokVersion = "1.18.30"
     const val jetbrainsAnnotations = "23.0.0"
