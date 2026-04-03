d1ee917e7d445285c0cd9ae4607ddfcd023a1366
Nathaniel <natruf@gmail.com>
2026-04-03 09:20:19 -0500

    #### **buildSrc: Update rlVersion dependency**
    - **`buildSrc/src/main/kotlin/Dependencies.kt`**
      - Bumped `rlVersion` from `1.12.22` to `1.12.23`.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object Dependencies {
     const val releaseVersion = "1.1.23"
-    const val rlVersion = "1.12.22"
+    const val rlVersion = "1.12.23"
     const val scalaVersion = "3.7.4"
     const val lombokVersion = "1.18.30"
     const val jetbrainsAnnotations = "23.0.0"
