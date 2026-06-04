90b4a23fd73e06a28c49fd6776f7c673e09795a7
Nathaniel <natruf@gmail.com>
2026-06-04 14:41:02 -0500

    Bump dependency versions: `releaseVersion` to `1.1.25`, `rlVersion` to `1.12.28`

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,8 +24,8 @@
  */
 
 object Dependencies {
-    const val releaseVersion = "1.1.24"
-    const val rlVersion = "1.12.27"
+    const val releaseVersion = "1.1.25"
+    const val rlVersion = "1.12.28"
     const val scalaVersion = "3.7.4"
     const val lombokVersion = "1.18.30"
     const val jetbrainsAnnotations = "23.0.0"
