c99b793b897040c9407076d7d85e05e58ecf9dea
Nathaniel <natruf@gmail.com>
2026-09-04 09:56:14 -0500

    Bump `rlVersion` to `1.12.39-SNAPSHOT` in `Dependencies.kt`.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -25,7 +25,7 @@
 
 object Dependencies {
     const val releaseVersion = "1.1.25"
-    const val rlVersion = "1.12.38-SNAPSHOT"
+    const val rlVersion = "1.12.39-SNAPSHOT"
     const val scalaVersion = "3.7.4"
     const val lombokVersion = "1.18.30"
     const val jetbrainsAnnotations = "23.0.0"
